0839a24e7c4ae2e2e3558a58bc62acaf8285d641
Dustin Machi <dmachi@virginia.edu>
2020-10-11 22:34:46 -0400

    fix total count

--- index.js
+++ index.js
@@ -150,29 +150,6 @@ Store.prototype.query=function(query, opts){
 		return results;
 	}
 
-	// request full recordset length
-	// N.B. due to collection.count doesn't respect meta.skip and meta.limit
-	// we have to correct returned totalCount manually.
-	// totalCount will be the minimum of unlimited query length and the limit itself
-	var totalCountPromise;
-	if (meta.totalCount){
-		totalCountPromise = (meta.totalCount)
-	}else{
-		totalCountPromise = when(collection.count(search), function(totalCount){
-			//console.log("TotalCount: ", totalCount);
-			totalCount -= meta.lastSkip;
-			if (!totalCount || (totalCount < 0))
-				totalCount = 0;
-			if (meta.lastLimit < totalCount)
-				totalCount = meta.lastLimit;
-			// N.B. just like in rql/js-array
-			meta.totalCount = Math.min(totalCount, typeof meta.totalCount === "number" ? meta.totalCount : Infinity); 
-			//return Math.min(totalCount, typeof meta.totalCount === "number" ? meta.totalCount : Infinity);
-			//console.log("Cur Meta: ", meta);
-			return meta.totalCount;
-		}) 
-	}
-
 	//console.log("SEARCH: ", search);
 	//console.log("META: ", meta);
 
@@ -206,17 +183,20 @@ Store.prototype.query=function(query, opts){
 				}
 			}
 			// total count
-			when(totalCountPromise, function(result){
-			var metadata = {}
+			// when(totalCountPromise, function(result){
+			cursor.count().then((tc)=>{
+				var metadata = {}
 				metadata.count = results.length;
-				metadata.start = meta.skip;
-				metadata.end = meta.skip + results.count;
-				metadata.totalCount = results.tc;
+				metadata.start = meta.skip||0;
+				metadata.end = (meta.skip||0) + (results.length-1)
+				metadata.totalCount = tc;
 
 				debug("MongoDB Store Results: ", results)
 				debug("   Result Meta: ", metadata);
 				deferred.resolve(new Result(results,metadata))
-			});
+			})
+	
+			// });
 		});
 
 	}
@@ -332,10 +312,10 @@ Store.prototype.delete=function(id,opts){
 	var collection = this.client.collection(this.collectionId || this.id);
 	var search = {}
 	search[this.primaryKey]=id
-	console.log("Search: ", search);
+	// console.log("Search: ", search);
 	collection.deleteOne(search, function(err, result){
 		if (err) return deferred.reject(err);
-		console.log("Delete Result: ", result);
+		// console.log("Delete Result: ", result);
 		deferred.resolve(new Result(true));
 	});
 	return deferred;
@@ -363,179 +343,5 @@ function parse(query,directives){
 	return mq;
 }
 
-function parseOrg(query, directives){
-	// parse string to parsed terms
-	if(typeof query === "string"){
-		// handle $-parameters
-		// TODO: consider security issues
-		//// N.B. considered, treated as evil, bump
-		//throw new URIError("Sorry, we don't allow raw querystrings. Please, provide the parsed terms instead");
-		if (directives && directives.parameters) {
-			query = query.replace(/\$[1-9]/g, function(param){
-				return directives.parameters[param.substring(1) - 1];
-			});
-		}
-		// poorman regexp? *foo, bar*
-		/***v = (v.charAt(0) != '*') ? '^' + v : v.substring(1);
-		v = (v.slice(-1) != '*') ? v + '$' : v.substring(0, v.length-1);***/
-		query = RQ.parseQuery(query);
-	}
-	var options = {
-		skip: 0,
-		limit: +Infinity,
-		lastSkip: 0,
-		lastLimit: +Infinity
-	};
-	var search = {};
-//	var needBulkFetch = directives && directives.postprocess; // whether to fetch whole dataset to process it here
-//if (!needBulkFetch) {
-
-	function walk(name, terms) {
-		console.log("Walking: ", name, "terms: ", terms);
-		// valid funcs
-		var valid_funcs = ['lt','lte','gt','gte','ne','in','nin','not','mod','all','size','exists','type','elemMatch'];
-		// funcs which definitely require array arguments
-		var requires_array = ['in','nin','all','mod'];
-		// funcs acting as operators
-		var valid_operators = ['or', 'and'];//, 'xor'];
-		// compiled search conditions
-		var search = {};
-		// iterate over terms
-		terms.forEach(function(term){
-			console.log("Check Term: ", term);
-			var func = term.name;
-			var args = term.args;
-			// ignore bad terms
-			// N.B. this filters quirky terms such as for ?or(1,2) -- term here is a plain value
-			if (!func || !args) return;
-			//dir(['W:', func, args]);
-			// process well-known functions
-			// http://www.mongodb.org/display/DOCS/Querying
-			if (func == 'sort' && args.length > 0) {
-				options.sort = args.map(function(sortAttribute){
-					var firstChar = sortAttribute.charAt(0);
-					var orderDir = 'ascending';
-					if (firstChar == '-' || firstChar == '+') {
-						if (firstChar == '-') {
-							orderDir = 'descending';
-						}
-						sortAttribute = sortAttribute.substring(1);
-					}
-					return [sortAttribute, orderDir];
-				});
-			} else if (func == 'select') {
-				options.fields = args;
-			} else if (func == 'values') {
-				options.unhash = true;
-				options.fields = args;
-				// N.B. mongo has $slice but so far we don't allow it
-			} else if (func == "distinct") {
-				options.distinct = args[0] || false;
-			/*} else if (func == 'slice') {
-				options[args.shift()] = {'$slice': args.length > 1 ? args : args[0]};*/
-			} else if (func == 'limit') {
-				// we calculate limit(s) combination
-				options.lastSkip = options.skip;
-				options.lastLimit = options.limit;
-				// TODO: validate args, negative args
-				var l = args[0] || Infinity, s = args[1] || 0;
-				// N.B: so far the last seen limit() contains Infinity
-				options.totalCount = args[2];
-				if (l <= 0) l = 0;
-				if (s > 0) options.skip += s, options.limit -= s;
-				if (l < options.limit) options.limit = l;
-				// grouping
-			} else if (func == 'group') {
-				// TODO:
-				// nested terms? -> recurse
-			} else if (args[0] && typeof args[0] === 'object') {
-				console.log("WALKING OBJECT: ", args);
-				console.log("func: ", func);
-				console.log("Valid operators: ", valid_operators);
-				if (valid_operators.indexOf(func) > -1){
-					if (func=="and") {
-						search['$'+func] = [walk(func, args)];
-					}else{
-						console.log("else walk(args): ", walk(args));
-						search["$"+func] = walk(func,args);
-					}
-				}else{
-					console.log("Func: ", func, " is not a valid operator");
-				}
-				// N.B. here we encountered a custom function
-				// ...
-				// structured query syntax
-				// http://www.mongodb.org/display/DOCS/Advanced+Queries
-			} else {
-				//dir(['F:', func, args]);
-				// mongo specialty
-				if (func == 'le') func = 'lte';
-				else if (func == 'ge') func = 'gte';
-				// the args[0] is the name of the property
-				var key = args.shift();
-				// the rest args are parameters to func()
-				if (requires_array.indexOf(func) >= 0) {
-					args = args[0];
-				} else {
-					// FIXME: do we really need to .join()?!
-					args = args.length == 1 ? args[0] : args.join();
-				}
-				// regexps:
-				if (typeof args === 'string' && args.indexOf('re:') === 0)
-				args = new RegExp(args.substr(3), 'i');
-				// regexp inequality means negation of equality
-				if (func == 'ne' && args instanceof RegExp) {
-					func = 'not';
-				}
-				// TODO: contains() can be used as poorman regexp
-				// E.g. contains(prop,a,bb,ccc) means prop.indexOf('a') >= 0 || prop.indexOf('bb') >= 0 || prop.indexOf('ccc') >= 0
-				//if (func == 'contains') {
-				//	// ...
-				//}
-				// valid functions are prepended with $
-				if (valid_funcs.indexOf(func) > -1) {
-					func = '$'+func;
-				}
-				// $or requires an array of conditions
-				// N.B. $or is said available for mongodb >= 1.5.1
-				console.log("name: ", name);
-				if (name == 'or') {
-					if (!(search instanceof Array))
-						search = [];
-					var x = {};
-					x[func == 'eq' ? key : func] = args;
-					search.push(x);
-					// other functions pack conditions into object
-				} else {
-					// several conditions on the same property is merged into one object condition
-					console.log("search key: ", key, "search[key]=",search[key]);
-					if (search[key] === undefined)
-						search[key] = {};
-					if (search[key] instanceof Object && !(search[key] instanceof Array)){
-						console.log("Add Function Args:" ,args);
-						//if (false){
-						//if (args.name) {
-						//	console.log("args.name: ", args.name, " args.args: ", args.args);
-						//	search[key][func] = walk("and",[args]);
-						//	console.log("search[" + key + "][" + func + "]", search[key][func]);
-						//}else{
-							search[key][func] = args;
-						//}
-					}
-					// equality cancels all other conditions
-					if (func == 'eq')
-						search[key] = args;
-				}
-			}
-			// TODO: add support for query expressions as Javascript
-		});
-		console.log("Return Search: ", search);
-		return search;
-	}
-	console.log(['Q:',query]);
-	search = walk(query.name, query.args);
-	console.log(['S:',search]);
-	return [options, search];
-}
 
 
